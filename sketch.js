4c1e6e9f74d172d01a7d614c159c3cbcf4f92675
sujoyghosh18 <69069562+sujoyghosh18@users.noreply.github.com>
2020-09-07 20:48:48 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -4,6 +4,8 @@ var speed,weight;
 
 function setup() { 
   createCanvas(1600,400); 
+  speed=random(55,90);
+  weight=random(400,1500);
   car = createSprite(50, 200, 50, 50); 
   car.shapeColor = "white"; 
   car.debug = true; 
@@ -12,10 +14,7 @@ function setup() {
   wall = createSprite(1500, 200,60,height/2); 
   wall.shapeColor=color(80,80,80); 
   wall.debug = true; 
-
-  speed=random(55,90);
-  weight=random(400,1500);
-} 
+ } 
   
   function draw() { 
     background(255,250,250); 
